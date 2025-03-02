de147f71e39b61a14e379e703536b1b6b42e6a13
Rafa Fadil Aras <rafafadilaras@gmail.com>
2025-03-02 18:59:45 +0700

    modify for update data

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -8,12 +8,15 @@ use Illuminate\Support\Facades\DB;
 class KategoriController extends Controller
 {
     public function index() {
-        $data = [
-                'kategori_kode'=>'SNK',
-                'kategori_nama'=> 'Snack/Makanan RIngan',
-                'created_at'=> now()
-            ];
-            DB::table('m_kategori')->insert($data);
-            return 'Insert data baru berhasil';
+        // $data = [
+        //         'kategori_kode'=>'SNK',
+        //         'kategori_nama'=> 'Snack/Makanan RIngan',
+        //         'created_at'=> now()
+        //     ];
+        //     DB::table('m_kategori')->insert($data);
+        //     return 'Insert data baru berhasil';
+
+        $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
+        return 'Update data berhasil. Jumlah data yang diupdate: '. $row. 'baris';
     }
 }
