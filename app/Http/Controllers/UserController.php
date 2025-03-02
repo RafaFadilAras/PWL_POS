f787085e5971ec18c320ea78656ff3dd66b72615
Rafa Fadil Aras <rafafadilaras@gmail.com>
2025-03-02 19:52:18 +0700

    modify UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\UserModel;
-use illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index() {
+
+        $data = [
+            'username' => 'customer-1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'),
+            'level_id' => 4
+        ];
+        
+        UserModel::insert($data);
+
         $user = UserModel::all();
         return view('user', ['data' => $user]);
+
     }
 }
