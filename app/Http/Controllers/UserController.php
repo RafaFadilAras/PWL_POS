e34878a1eaa5e2049a9ea70bf8b62647bcd2974c
Rafa Fadil Aras <rafafadilaras@gmail.com>
2025-03-08 19:47:38 +0700

    Praktikum 1.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,11 +12,11 @@ class UserController extends Controller
 
         $data = [
             'level_id' => 2,
-            'username' => 'manager_dua',
-            'nama' => 'Manager 2',
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
             'password' => Hash::make('12345')   
         ];
-        //UserModel::create($data);
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
