7030f7eb745be1a6ea43b36a159b46d466ee7418
Rafa Fadil Aras <rafafadilaras@gmail.com>
2025-03-08 21:15:25 +0700

    Praktikum 2.4 - firstOrNew

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,14 +35,20 @@ class UserController extends Controller
         // $user = UserModel::where('level_id', 2)->count();
         //dd($user);
 
-        $user = UserModel::firstOrCreate(
-[
-                'username' => 'manager22',
-                'nama' => 'Manager Dua Dua',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
+//         $user = UserModel::firstOrCreate(
+// [
+//                 'username' => 'manager22',
+//                 'nama' => 'Manager Dua Dua',
+//                 'password' => Hash::make('12345'),
+//                 'level_id' => 2
+//             ],
+//          );
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager',
+                'nama' => 'Manager',
             ],
-         );
+        );
         return  view('user', ['data' => $user]);
     }
 }
