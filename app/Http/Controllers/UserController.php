6279c53699e4a8917c8fd40d640a49e876baab1d
Rafa Fadil Aras <rafafadilaras@gmail.com>
2025-03-08 20:04:35 +0700

    Praktikum 2.1 - Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,15 +10,18 @@ class UserController extends Controller
 {
     public function index() {
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')   
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')   
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::find(1);
         return view('user', ['data' => $user]);
 
     }
