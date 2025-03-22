394b7d3f4ce550b8f51352e103caeed420e65d9b
Rafa Fadil Aras <rafafadilaras@gmail.com>
2025-03-22 09:02:51 +0700

    Jobsheet 6 - Praktikum 1. Modal Ajax Data User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\LevelModel;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -79,6 +80,13 @@ class UserController extends Controller
          ]);
     }
 
+    public function create_ajax() {
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('user.create_ajax')
+            ->with('level', $level);
+    }
+
     // Mentimpan data user baru
     public function store(Request $request)
      {
@@ -99,6 +107,35 @@ class UserController extends Controller
 
          return redirect('/user')->with('success', 'Data user berhasil disimpan');
      }
+
+    public function store_ajax(Request $request) {
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|string|min:3|unique:m_user,username',
+                'nama' => 'required|string|max:100',
+                'password' => 'required|min:6'
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors()
+                ]);
+            }
+
+            UserModel::create($request->all());
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Data user berhasil disimpan'
+            ]);
+        }
+        redirect('/');
+    }
     
     // Menampilkan detail user
     public function show(string $id)
