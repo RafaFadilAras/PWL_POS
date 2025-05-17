dcdcbae838c3d6d3f017ecc4a281cb27931c20dd
rfclz <rafafadilaras@gmail.com>
2025-05-17 12:15:29 +0700

    Tugas 1.1 - Implementasi API untuk upload file pada tabel barang

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -4,20 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class BarangModel extends Model
 {
     protected $table = 'm_barang';
-     protected $primaryKey = 'barang_id';
-     protected $fillable = ['barang_id', 'kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual'];
- 
-     public function kategori()
-     {
-         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
-     }
- 
-     public function stok()
- {
-     return $this->hasOne(StokModel::class, 'barang_id', 'barang_id');
- }
+    protected $primaryKey = 'barang_id';
+    protected $fillable = ['barang_id', 'kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual', 'image'];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    public function kategori()
+    {
+        return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
+    }
+
+    public function stok()
+    {
+        return $this->hasOne(StokModel::class, 'barang_id', 'barang_id');
+    }
 }
